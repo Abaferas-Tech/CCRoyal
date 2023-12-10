3658df6bc5f3ce04c661c19f540c02ac049838db
abaferas <wasfyelgamed@gmail.com>
2023-12-10 16:54:45 +0200

    start implementing the logic

--- app/src/main/java/com/abaferas/cardcalc/ui/screen/HomeScreenInteraction.kt
+++ app/src/main/java/com/abaferas/cardcalc/ui/screen/HomeScreenInteraction.kt
@@ -6,6 +6,10 @@ interface HomeScreenInteraction {
 
     fun onSelectType(index: Int, type: Type)
     fun onAmountChange(amount: String)
+    fun onCurrentLevelChange(level: String)
     fun onClearAmount()
+    fun onClearLevel()
+
+    fun onCalculateClick()
 
 }
